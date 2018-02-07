4283f9cd83dfb1bcd993949a1a1e4a1c86597d23
Rigel Kent <par@rigelk.eu>
2018-02-07 15:47:55 +0100

    shell install script improvement
    Signed-off-by: neze <clement@neze.fr>

--- install/unix.sh
+++ install/unix.sh
@@ -1,17 +1,68 @@
-#!/bin/sh
-#cd "$(readlink -m "$0/../../")" # chdir to the root directory of the package archive
-cd $(dirname $0)
-cd ..
+#!/bin/env sh
+set -euo pipefail
 
-TEXMFLOCAL="$(kpsewhich -var-value TEXMFLOCAL)"
+#/ Usage:
+#/ Description: install TPT's beamer theme on a POSIX-compliant machine
+#/ Options:
+#/   --global: Installs for all user (needs root rights)
+#/   --help: Display this help message
+usage() { grep '^#/' "$0" | cut -c4- ; exit 0 ; }
+
+readonly LOG_FILE="/tmp/install-telecom-beamer.log"
+info()    { echo "[INFO]    $*" | tee -a "$LOG_FILE" >&2 ; }
+warning() { echo "[WARNING] $*" | tee -a "$LOG_FILE" >&2 ; }
+error()   { echo "[ERROR]   $*" | tee -a "$LOG_FILE" >&2 ; }
+fatal()   { echo "[FATAL]   $*" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
+check_rights()  { 
+  if [ "$(id -u)" -ne 0 ];
+  then
+    error "Not sufficient rights to $1"
+    exit 2
+  fi
+}
+
+TEXMFLOCAL="$HOME/texmf"
 PACKAGE="$TEXMFLOCAL/tex/latex/beamerx/"
 FONTS="$TEXMFLOCAL/fonts/truetype/"
 PERMS="Dg+s,ug+w,o-w,+X,+r"
-mkdir -p "$PACKAGE" "$FONTS"
-printf "%s> Installing source files...%s\n" "$(tput setaf 2)" "$(tput sgr0)"
-rsync --recursive --exclude-from=install/.exclude --delete-excluded --chmod=$PERMS source/ $PACKAGE/
-printf "%s> Installing font files...%s\n" "$(tput setaf 2)" "$(tput sgr0)"
-rsync --recursive --update --chmod=$PERMS fonts/ $FONTS/
-printf "%s> Updating database...%s\n" "$(tput setaf 2)" "$(tput sgr0)"
-{ texhash || mktexlsr; } >/dev/null
-printf "%sDONE !%s\n" "$(tput setaf 2)" "$(tput sgr0)"
+
+ensure_tree_exists() {
+  mkdir -p "$PACKAGE" "$FONTS"
+}
+
+install_in_tree() {
+  info "Installing source files in $PACKAGE"
+  rsync --recursive --exclude-from=install/.exclude --delete-excluded --chmod="$PERMS" source/ "$PACKAGE/"
+  info "Installing font files in $FONTS"
+  rsync --recursive --update --chmod="$PERMS" fonts/ "$FONTS/"
+}
+
+update_database() {
+  info "Updating database…"
+  texhash || mktexlsr || warning "Couldn't update database."
+}
+
+cleanup() {
+  case "$?" in
+    1)
+      fatal "Install could not complete."
+      ;;
+    2)
+      warning "Install has completed, with errors."
+      ;;
+    *)
+      info "Install has completed."
+      ;;
+  esac
+}
+trap cleanup EXIT
+
+#Parsing options
+expr "$*" : ".*--help" > /dev/null && usage
+expr "$*" : ".*--global" > /dev/null && TEXMFLOCAL="$(kpsewhich -var-value TEXMFLOCAL)" && check_rights "access $TEXMFLOCAL"
+
+#Install process
+ensure_tree_exists
+install_in_tree
+update_database
+
